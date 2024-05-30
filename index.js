25aa8b677c64b3099884fc6978d4ac36f2d35ccd
Pandu Norsya'bani <pandunorsyabani@gmail.com>
2024-05-30 11:44:01 +0800

    change the frontend design

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const link = document.getElementById("link");
 btn.addEventListener("click", function () {
   const url = link.value.replace("/edit#gid=", "/export?format=xlsx&gid=");
   window.open(url, "_blank").focus();
+  link.value = "";
 });
